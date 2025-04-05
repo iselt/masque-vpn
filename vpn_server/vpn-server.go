5490ffd75392d08097cd7c3b4ad8102ce84505f4
unknown <kyle_hoo@qq.com>
2025-04-05 10:50:48 +0800

    Configure Ciphersuites

--- vpn_server/vpn-server.go
+++ vpn_server/vpn-server.go
@@ -99,6 +99,11 @@ func main() {
 	}
 	tlsConfig := http3.ConfigureTLSConfig(&tls.Config{
 		Certificates: []tls.Certificate{cert},
+		CipherSuites: []uint16{
+			// tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			// tls.TLS_CHACHA20_POLY1305_SHA256,
+		},
 	})
 
 	// --- QUIC 监听器 ---
